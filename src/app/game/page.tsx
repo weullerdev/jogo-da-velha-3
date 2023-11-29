1c6855782d9438daf41fb8a140ac6a1c479e64e2
Weuller Camargo <weuller.camargo@go4digital.com.br>
2023-11-28 22:09:29 -0300

    Ajustes visuais

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -21,6 +21,7 @@ export default function Game() {
   const { turn } = useSocket()
   const [squareGame, setSquareGame] = useState<SquareGame[]>([])
   const [turnGame, setTurnGame] = useState<'x' | 'o'>('x')
+  const [playerWin, setPlayerWin] = useState(false)
 
   const ENDPOINT = 'http://localhost:3333'
 
@@ -60,23 +61,23 @@ export default function Game() {
     // Verificar linhas
     for (let i = 0; i < 7; i += 3) {
       if (squareGame[i].value === currentTurn && squareGame[i + 1].value === currentTurn && squareGame[i + 2].value === currentTurn) {
-        socket.emit('endGame', currentTurn)
+        setPlayerWin(true)
       }
     }
 
     // Verificar colunas
     for (let i = 0; i < 3; i++) {
       if (squareGame[i].value === currentTurn && squareGame[i + 3].value === currentTurn && squareGame[i + 6].value === currentTurn) {
-        socket.emit('endGame', currentTurn)
+        setPlayerWin(true)
       }
     }
 
     // Verificar diagonais
     if (squareGame[0].value === currentTurn && squareGame[4].value === currentTurn && squareGame[8].value === currentTurn) {
-      socket.emit('endGame', currentTurn)
+      setPlayerWin(true)
     }
     if (squareGame[2].value === currentTurn && squareGame[4].value === currentTurn && squareGame[6].value === currentTurn) {
-      socket.emit('endGame', currentTurn)
+      setPlayerWin(true)
     }
   }
 
@@ -86,6 +87,14 @@ export default function Game() {
   }, [])
 
   useEffect(() => {
+    if(playerWin){
+      win(turnGame === 'o' ? 'x' : 'o')
+      socket.emit('createGame')
+      setPlayerWin(false)
+    }
+  },[playerWin])
+
+  useEffect(() => {
     socket.on('updateGame', (game) => {
       setSquareGame(game.value) 
       setTurnGame(game.turn)
@@ -93,16 +102,34 @@ export default function Game() {
         winConditions(game.value)
       }
     })
-    socket.on('winner', (element) => win(element))
+    // socket.on('winner', (element) => win(element))
   })
 
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center gap-4'>
-      <h1 className='text-white'>Jogo da Velha 3</h1>
-      <div className='flex justify-between w-[calc(128px*3+64px)]'>
-        <div>X O</div>
-        <div>Turno {turnGame?.toUpperCase()}</div>
-        <button onClick={() => socket.emit('reset')}>Recomeçar</button>
+      <div className='grid grid-cols-3 w-[calc(128px*3+64px)] h-10'>
+        <div className='flex items-center gap-2'>
+          <Image src={PLAYER_X} alt='' width={25} height={25} />
+          <Image src={PLAYER_O} alt='' width={25} height={25} />
+        </div>
+        <div className='flex items-center justify-center'>
+          <div className='flex items-center w-28 h-9 gap-2 rounded-lg bg-[#2c4a58] py-1 px-3'>
+            <span className='text-white text-lg'>Turno</span> 
+            {turnGame === 'x' ? ( 
+              <Image src={PLAYER_X} alt='' width={25} height={25} />
+            ) : ( 
+              <Image src={PLAYER_O} alt='' width={25} height={25} />
+            )}
+          </div>
+        </div>
+        <div className='flex justify-end items-center gap-2'>
+          <button 
+            className='w-28 h-9 rounded-lg text-base text-white bg-[#7d8d94] hover:bg-[#515c61] transition ease-in-out duration-300'
+            onClick={() => socket.emit('createGame', 'x')}
+          >
+            Recomeçar
+          </button>
+        </div>
       </div>
       <div className='grid grid-cols-3 w-[calc(128px*3+64px)] gap-y-6 gap-x-8'>
         {squareGame?.map((x, i) => (
@@ -118,10 +145,26 @@ export default function Game() {
           </div>
         ))}
       </div>
-      <div className='flex justify-between w-[calc(128px*3+64px)]'>
-        <div>Wins X</div>
-        <button>Resetar Wins</button>
-        <div>Wins O</div>
+      <div className='grid grid-cols-3 w-[calc(128px*3+64px)] h-14'>
+        <div className='flex items-center'>
+          <div className='flex flex-col items-center justify-around w-28 h-13 rounded-lg bg-[#31c4be] bg-opacity-60'>
+            <span className='text-white text-lg'>Wins X</span>
+            <div className='text-white text-lg'>1</div>
+          </div>
+        </div>
+        <div className='flex justify-center items-center gap-2'>
+          <button
+            className='w-28 h-9 rounded-lg text-base text-white bg-[#7d8d94] hover:bg-[#515c61] transition ease-in-out duration-300'
+          >
+            Resetar
+          </button>
+        </div>
+        <div className='flex items-center justify-end'>
+          <div className='flex flex-col items-center justify-around w-28 h-13 rounded-lg bg-[#f2b237] bg-opacity-60'>
+            <span className='text-white text-lg'>Wins O</span>
+            <div className='text-white text-lg'>1</div>
+          </div>
+        </div>
       </div>
     </div>
   )
